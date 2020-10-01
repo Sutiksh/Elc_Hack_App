51dd02c167bd6f27315bb85d6560ca72a773b88e
Sutiksh Verma <verma.sutiksh27@gmail.com>
2020-10-01 09:39:05 +0530

    added home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:Elc_Hack_App/ui/pages/home.dart';
+
+main() {
+  runApp(
+    MaterialApp(
+      title: 'ELC Hack App',
+      home: Home(),
+    ),
+  );
+}
